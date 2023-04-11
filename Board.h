02ae57ca057e2f709e7dc35e4ace80196b75f670
Ngoc Linh Pham <121231072+novas2061@users.noreply.github.com>
2023-04-11 22:34:48 +0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -2,12 +2,12 @@
 
 void deletePossibleLines(std::vector<std::vector<int>>&);
 void deleteLine(std::vector<std::vector<int>> &, int);
-bool isToStore(int posX, int posY, std::vector<std::vector<int>> storingBoard);
-bool isFreeBlock(int posX, int posY, std::vector<std::vector<int>> storingBoard);
-bool isPossibleMove (int posX, int posY, std::vector<std::vector<int>> storingBoard);
-int posInPixels(int pos);
-void updateStoringBoard(int posX, int posY, std::vector<std::vector<int>>&);
+bool isToStore(int, int, std::vector<std::vector<int>>);
+bool isFreeBlock(int, int, std::vector<std::vector<int>>);
+bool isPossibleMove (int, int, std::vector<std::vector<int>>);
+int posInPixels(int);
+void updateStoringBoard(int, int, std::vector<std::vector<int>>&);
 void initBoard(std::vector<std::vector<int>>&);
-void updateMovingBoard(int posX, int posY, std::vector<std::vector<int>>&);
-void dropTheBlock(int posX, int& posY, std::vector<std::vector<int>> storingBoard);
+void updateMovingBoard(int, int, std::vector<std::vector<int>>&);
+void dropTheBlock(int, int&, std::vector<std::vector<int>>);
 bool isGameOver(std::vector<std::vector<int>>);
